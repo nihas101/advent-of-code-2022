a0b04f10da519f1654a3c95739953f67bc7b279f
Nikita Hasert <nikitahasert@gmail.com>
2022-12-05 22:02:20 +0100

    Day 05: Add solution

--- src/advent_of_code_2022/day05.clj
+++ src/advent_of_code_2022/day05.clj
@@ -1,3 +1,61 @@
 (ns advent-of-code-2022.day05
   (:require
-   [advent-of-code-2022.utils :as u]))
+   [advent-of-code-2022.utils :as u]
+   [clojure.string :as string]))
+
+(defn- split-stack-lines [crates-line]
+  (rest
+   (string/split
+    (string/replace (string/replace crates-line #"\s{4}\s?" "[]")
+                    #"\s+" "")
+    #"\]\[|\[|\]")))
+
+(defn- stack-line->crate+index [crates-line]
+  (map-indexed (fn [^long idx crate] [(inc idx) crate]) crates-line))
+
+(defn- conj-to-stacks
+  ([] {})
+  ([crate-stacks] crate-stacks)
+  ([crate-stacks crates-line]
+   (reduce (fn [cs [idx c]]
+             (if (seq c)
+               (update cs idx (fnil conj '()) c)
+               cs))
+           crate-stacks crates-line)))
+
+(defn- parse-crate-stacks [crate-stacks]
+  (transduce
+   (comp
+    (map split-stack-lines)
+    (map stack-line->crate+index))
+   conj-to-stacks
+   (rest (reverse (u/split-sections crate-stacks u/line-endings)))))
+
+(defn- parse-rearrangement-procedure [rearrangement-procedure]
+  (mapv (fn [operation]
+          (mapv #(Long/parseLong %)
+                (rest (string/split operation #"move\s|\sfrom\s|\sto\s"))))
+        (string/split rearrangement-procedure u/line-endings)))
+
+(defn parse-input [input]
+  (let [[crate-stacks rearrangement-procedure] (u/split-sections input)]
+    {:stacks (parse-crate-stacks crate-stacks)
+     :procedure (parse-rearrangement-procedure rearrangement-procedure)}))
+
+(defn- day05 [crate-concat]
+  (fn [{:keys [stacks procedure]}]
+    (transduce
+     (comp (map second)
+           (map first))
+     str
+     (->> procedure
+          (reduce (fn [stacks [q s t]]
+                    (-> stacks
+                        (update ,,, t crate-concat (take q (get stacks s)))
+                        (update ,,, s (partial drop q))))
+                  stacks)
+          (sort-by first)))))
+
+(defonce day05-1 (day05 into))
+
+(defonce day05-2 (day05 (fn [stack crates] (concat crates stack))))
---
+++ test/advent_of_code_2022/day05_test.clj
@@ -0,0 +1,32 @@
+(ns advent-of-code-2022.day05-test
+  (:require
+   [clojure.test :refer :all]
+   [advent-of-code-2022.day05 :refer :all]))
+
+(def ^:private example-input (parse-input "    [D]    
+[N] [C]    
+[Z] [M] [P]
+ 1   2   3 
+
+move 1 from 2 to 1
+move 3 from 1 to 3
+move 2 from 2 to 1
+move 1 from 1 to 2"))
+
+(def ^:private input (parse-input (slurp "resources/day05.txt")))
+
+(deftest day05-1-example-test
+  (testing "day05-1 example"
+    (is (= "CMZ" (day05-1 example-input)))))
+
+(deftest day05-1-test
+  (testing "day05-1 example"
+    (is (= "TPGVQPFDH" (day05-1 input)))))
+
+(deftest day05-2-example-test
+  (testing "day05-2 example"
+    (is (= "MCD" (day05-2 example-input)))))
+
+(deftest day05-2-test
+  (testing "day05-2 example"
+    (is (= "DMRDFRHHH" (day05-2 input)))))
